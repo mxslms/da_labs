c9d450807587e6026b96a354436c6236ddbf390a
mxslms <mkonwsh@gmail.com>
2025-03-16 19:26:03 -0400

    made some changes

--- da_mod3_lab1.py
+++ da_mod3_lab1.py
@@ -5,9 +5,9 @@ import matplotlib.pyplot as plt
 
 filepath='https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-DA0101EN-SkillsNetwork/labs/Data%20files/automobileEDA.csv'
 df = pd.read_csv(filepath, header=0)
-'''
-print(df.head())
 
+print(df.head())
+'''
 sdfsdfsdfsdfsd
 
 print(df['peak-rpm'].dtypes) 
@@ -51,3 +51,52 @@ plt.show()
 sns.boxplot(x="drive-wheels", y="price", data=df)
 plt.show()
 '''
+df.describe(include=['object'])
+df.describe()
+drive_wheels_counts = df['drive-wheels'].value_counts().to_frame()
+drive_wheels_counts.reset_index(inplace=True)
+drive_wheels_counts=drive_wheels_counts.rename(columns={'drive-wheels': 'value_counts'})
+drive_wheels_counts
+
+
+engine_loc_counts = df['engine-location'].value_counts().to_frame()
+engine_loc_counts.rename(columns={'engine-location': 'value_counts'}, inplace=True)
+engine_loc_counts.index.name = 'engine-location'
+engine_loc_counts.head(10)
+
+df['drive-wheels'].unique()
+
+df_group_one = df[['drive-wheels','body-style','price']]
+
+df_grouped = df_group_one.groupby(['drive-wheels'], as_index=False).agg({'price': 'mean'})
+df_grouped
+
+df_gptest = df[['drive-wheels','body-style','price']]
+grouped_test1 = df_gptest.groupby(['drive-wheels','body-style'],as_index=False).mean()
+grouped_test1
+
+grouped_pivot = grouped_test1.pivot(index='drive-wheels',columns='body-style')
+grouped_pivot
+
+grouped_pivot = grouped_pivot.fillna(0) #fill missing values with 0
+grouped_pivot
+
+sns.heatmap(grouped_pivot, annot=True, cmap='YlGnBu')  # annot=True shows values in cells
+plt.show()
+
+df_grpstyle = df[['body-style','price']]
+df_grpstyle = df_grpstyle.groupby(['body-style'], as_index=False).mean()
+df_grpstyle 
+
+plt.pcolor(grouped_pivot, cmap='RdBu')
+plt.colorbar()
+plt.show()
+
+df.select_dtypes(include=['number']).corr()
+from scipy import stats
+pearson_coef, p_value = stats.pearsonr(df['wheel-base'], df['price'])
+print("The Pearson Correlation Coefficient is", pearson_coef, " with a P-value of P =", p_value)  
+
+pearson_coef, p_value = stats.pearsonr(df['width'], df['price'])
+print("The Pearson Correlation Coefficient is", pearson_coef, " with a P-value of P = ", p_value)  
+
