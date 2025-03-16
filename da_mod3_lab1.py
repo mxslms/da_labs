fa57a806b6d47b1db8ce8d23cf019ea0426aa084
mxslms <mkonwsh@gmail.com>
2025-03-16 17:00:17 -0400

    updating

--- da_mod3_lab1.py
+++ da_mod3_lab1.py
@@ -1,13 +1,14 @@
 import pandas as pd
 import numpy as np
 import seaborn as sns
+import matplotlib.pyplot as plt
 
 filepath='https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-DA0101EN-SkillsNetwork/labs/Data%20files/automobileEDA.csv'
 df = pd.read_csv(filepath, header=0)
-
+'''
 print(df.head())
 
-import matplotlib.pyplot as plt
+
 
 print(df['peak-rpm'].dtypes) 
 
@@ -18,5 +19,37 @@ plt.figure(figsize=(20,10))
 sns.heatmap(corr_matrix, annot=True), cmap='coolwarm', fmt='.2f', linewidths=0.5)
 plt.title('Correlation Heatmap')
 plt.show()
+corr_matrix
+df[['bore', 'stroke', 'compression-ratio', 'horsepower']].corr()    
 
-#editing branch 1 again lets see what is this????
+sns.regplot(x='engine-size', y='price', data=df)
+plt.ylim(0,)
+plt.show()
+
+df[["engine-size", "price"]].corr()
+df[["highway-mpg", "price"]].corr()
+sns.regplot(x='highway-mpg', y='price', data=df)
+plt.ylim(0,)
+plt.show()
+
+sns.regplot(x="peak-rpm", y="price", data=df)
+plt.ylim(0,)
+plt.show()
+df[['peak-rpm','price']].corr()
+
+sns.regplot(x="stroke", y="price", data=df)
+plt.ylim(0,)
+plt.show()
+df[['stroke','price']].corr()
+
+sns.boxplot(x="body-style", y="price", data=df)
+plt.show()
+
+sns.boxplot(x="engine-location", y="price", data=df)
+plt.show()
+
+sns.boxplot(x="drive-wheels", y="price", data=df)
+plt.show()
+'''
+print(df.describe(include=['object']))
+print('hello')
